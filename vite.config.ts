86e9da6b5232f9943741b09cc60cc25698569114
ISHOWO ASMAA KHADIJAH <asmaaishowo2912@gmail.com>
2024-07-13 21:13:34 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
       '/api': {
         target: 'https://timbu-get-all-products.reavdev.workers.dev/',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        
       },
     },
   },
